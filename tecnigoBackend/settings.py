11439cbc9628b258f2da0d3975a4c27ef2d126f3
Tomas Gonzalez <tomasgp14@hotmail.com>
2020-07-28 20:13:01 -0400

    changes

--- tecnigoBackend/settings.py
+++ tecnigoBackend/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     "tecnigo",
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     "graphene_django",
     "django.contrib.admin",
     "django.contrib.auth",
@@ -119,5 +120,18 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/1.11/howto/static-files/
+PROJECT_ROOT   =   os.path.join(os.path.abspath(__file__))
+STATIC_ROOT  =   os.path.join(PROJECT_ROOT, 'staticfiles')
+STATIC_URL = '/static/'
 
-STATIC_URL = "/static/"
+# Extra lookup directories for collectstatic to find static files
+STATICFILES_DIRS = (
+    os.path.join(PROJECT_ROOT, 'static'),
+)
+
+#  Add configuration for static files storage using whitenoise
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+ 
+
